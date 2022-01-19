01c201f9ee2d646f52bcf61a8e9e1fc1db0cdbd4
InezMallandain <inezvmallandain@gmail.com>
2022-01-19 11:12:49 +0200

    add dynamic background

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-const button = document.querySelector('.button');
-const input = document.querySelector('.input');
-const search = document.querySelector('.search');
+const button = document.querySelector(".button");
+const input = document.querySelector(".input");
+const search = document.querySelector(".search");
+const body = document.querySelector("body");
+button.addEventListener("click", () => {
+  search.classList.toggle("active");
+  input.focus();
+});
 
-button.addEventListener('click', () => {
-    search.classList.toggle('active');
-    input.focus()
-})
+input.addEventListener("keyup", function (event) {
+  document.body.style.backgroundImage = `url('https://source.unsplash.com/featured/?${event.target.value}')`;
+});
